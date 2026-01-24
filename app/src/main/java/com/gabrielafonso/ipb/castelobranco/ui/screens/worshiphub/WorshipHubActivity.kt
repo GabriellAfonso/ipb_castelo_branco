f50870dfad91b7580d30b557580da58ffdf76a39
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-23 22:23:56 -0300

    feat(ui): integrate WorshipHubViewModel into WorshipHubActivity and enhance PreviewSongsTable with fake data

--- app/src/main/java/com/gabrielafonso/ipb/castelobranco/ui/screens/worshiphub/WorshipHubActivity.kt
+++ app/src/main/java/com/gabrielafonso/ipb/castelobranco/ui/screens/worshiphub/WorshipHubActivity.kt
@@ -1,13 +1,20 @@
 package com.gabrielafonso.ipb.castelobranco.ui.screens.worshiphub
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.gabrielafonso.ipb.castelobranco.domain.model.SundaySet
+import com.gabrielafonso.ipb.castelobranco.domain.model.SundaySetItem
 import com.gabrielafonso.ipb.castelobranco.ui.screens.base.BaseActivity
+import com.gabrielafonso.ipb.castelobranco.ui.screens.base.BaseViewModelProvider
+import com.gabrielafonso.ipb.castelobranco.ui.screens.worshiphub.views.WorshipHubScreen
 import com.gabrielafonso.ipb.castelobranco.ui.theme.IPBCasteloBrancoTheme
 
 class WorshipHubActivity : BaseActivity() {
+
+    private val viewModel: WorshipHubViewModel by viewModels { BaseViewModelProvider.Factory }
 
     override fun onPreCreate(savedInstanceState: Bundle?) {
         super.onPreCreate(savedInstanceState)
@@ -23,7 +30,10 @@ class WorshipHubActivity : BaseActivity() {
     @Composable
     override fun ScreenContent() {
         val navController = rememberNavController()
-        WorshipHubNavGraph(navController = navController)
+        WorshipHubNavGraph(
+            navController = navController,
+            viewModel = viewModel
+        )
 //        WorshipHubScreen()
     }
 }
@@ -37,13 +47,29 @@ fun PreviewWorshipHub() {
 }
 
 
- @Preview(name = "SongsTable", showBackground = true)
- @Composable
- fun PreviewSongsTable() {
-     IPBCasteloBrancoTheme {
-         WorshipSongsTableScreen(onBack = {})
-     }
- }
+@Preview(name = "SongsTable", showBackground = true)
+@Composable
+fun PreviewSongsTable() {
+    val fakeSundays = listOf(
+        SundaySet(
+            date = "2026-01-21",
+            songs = listOf(
+                SundaySetItem(position = 1, title = "Música 1", tone = "C", artist = "Artista A"),
+                SundaySetItem(position = 2, title = "Música 2", tone = "G", artist = "Artista B")
+            )
+        )
+    )
+
+    IPBCasteloBrancoTheme {
+        WorshipSongsTableUi(
+            onBack = {},
+            sundays = fakeSundays,
+            topSongs = emptyList(),
+            topTones = emptyList()
+
+        )
+    }
+}
 // Exemplo para outras screens:
 // @Preview(name = "SongsTable", showBackground = true)
 // @Composable
@@ -52,3 +78,4 @@ fun PreviewWorshipHub() {
 //         SongsTableScreen(onBack = {})
 //     }
 // }
+
