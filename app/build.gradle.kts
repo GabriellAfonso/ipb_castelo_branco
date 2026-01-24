78a52fe4568f913dd59ef2cfcbe9c75a14a34d38
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-23 22:21:53 -0300

    feat(deps): add Room and Media3 database dependencies to build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
 
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -87,6 +88,7 @@ dependencies {
     implementation(libs.converter.gson)
     implementation(libs.androidx.compose.runtime.livedata)
     implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.media3.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -99,5 +101,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-kotlinx-serialization:3.0.0")
     implementation("androidx.navigation:navigation-compose:2.9.6")
 
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
 
 }
