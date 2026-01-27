6c393cd2a522f3cfa6f250f08964e2779ec8d66e
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-27 15:27:36 -0300

    feat(main): enhance MainViewModel to include hymnal and month schedule repositories

--- app/src/main/java/com/gabrielafonso/ipb/castelobranco/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/com/gabrielafonso/ipb/castelobranco/ui/screens/main/MainViewModel.kt
@@ -3,11 +3,15 @@ package com.gabrielafonso.ipb.castelobranco.ui.screens.main
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.gabrielafonso.ipb.castelobranco.data.repository.HymnalRepositoryImpl
+import com.gabrielafonso.ipb.castelobranco.data.repository.MonthScheduleRepositoryImpl
 import com.gabrielafonso.ipb.castelobranco.data.repository.SongsRepositoryImpl
 import kotlinx.coroutines.launch
 
 class MainViewModel(
-    private val repository: SongsRepositoryImpl
+    private val repository: SongsRepositoryImpl,
+    private val hymnalRepository: HymnalRepositoryImpl,
+    private val monthScheduleRepository: MonthScheduleRepositoryImpl
 ) : ViewModel() {
 
     companion object {
@@ -20,14 +24,32 @@ class MainViewModel(
 
     private fun preload() {
         viewModelScope.launch {
-            val ok = repository.refreshSongsBySunday()
-            repository.refreshTopSongs()
-            repository.refreshTopTones()
-            if (!ok) {
-                Log.w(TAG, "refreshSongsBySunday falhou")
-            } else {
-                Log.d(TAG, "refreshSongsBySunday sucesso")
-            }
+            val okSongsBySunday = repository.refreshSongsBySunday()
+            val okTopSongs = repository.refreshTopSongs()
+            val okTopTones = repository.refreshTopTones()
+            val okSuggestedSongs = repository.refreshSuggestedSongs()
+            val okHymnal = hymnalRepository.refreshHymnal()
+            val okMonthSchedule = monthScheduleRepository.refreshMonthSchedule()
+
+
+            if (!okSongsBySunday) Log.w(TAG, "refreshSongsBySunday falhou")
+            else Log.d(TAG, "refreshSongsBySunday sucesso")
+
+            if (!okTopSongs) Log.w(TAG, "refreshTopSongs falhou")
+            else Log.d(TAG, "refreshTopSongs sucesso")
+
+            if (!okTopTones) Log.w(TAG, "refreshTopTones falhou")
+            else Log.d(TAG, "refreshTopTones sucesso")
+
+            if (!okSuggestedSongs) Log.w(TAG, "refreshSuggestedSongs falhou")
+            else Log.d(TAG, "refreshSuggestedSongs sucesso")
+
+            if (!okHymnal) Log.w(TAG, "refreshHymnal falhou")
+            else Log.d(TAG, "refreshHymnal sucesso")
+
+            if (!okMonthSchedule) Log.w(TAG, "refreshMonthSchedule falhou")
+            else Log.d(TAG, "refreshMonthSchedule sucesso")
+
         }
     }
 }
