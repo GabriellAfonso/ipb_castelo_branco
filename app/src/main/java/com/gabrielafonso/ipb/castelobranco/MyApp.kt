40cff39e2b8ec64eb6127b5788c4cfd1a1e4956c
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-31 23:25:01 -0300

    feat(app): implement theme preferences for dark mode

--- app/src/main/java/com/gabrielafonso/ipb/castelobranco/MyApp.kt
+++ app/src/main/java/com/gabrielafonso/ipb/castelobranco/MyApp.kt
@@ -1,8 +1,26 @@
-// kotlin
+
 package com.gabrielafonso.ipb.castelobranco
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.gabrielafonso.ipb.castelobranco.data.local.ThemePreferences
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 @HiltAndroidApp
-class MyApp : Application()
+class MyApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        val prefs = ThemePreferences(applicationContext)
+        val prefDark = runBlocking { prefs.darkModeFlow.first() } // Boolean?
+
+        AppCompatDelegate.setDefaultNightMode(
+            when (prefDark) {
+                null -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                true -> AppCompatDelegate.MODE_NIGHT_YES
+                false -> AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
+}
