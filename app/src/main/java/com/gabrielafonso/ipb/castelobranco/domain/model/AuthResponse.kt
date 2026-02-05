67d4673f11fb2f00ad6dc46c1367854fc60c02f7
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-02-04 23:59:56 -0300

    refactor(di): remove AppContainer and AuthResponse classes

--- app/src/main/java/com/gabrielafonso/ipb/castelobranco/domain/model/AuthResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.gabrielafonso.ipb.castelobranco.domain.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AuthResponse(
-    val token: String,
-    val refresh: String
-)
