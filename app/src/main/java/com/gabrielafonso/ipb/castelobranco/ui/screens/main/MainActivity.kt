14aa1ab656fe7375f8eedbefee38a5b1ec3735e4
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-02-05 22:11:20 -0300

    chore(mainActivity): add enableEdgeToEdge import

--- app/src/main/java/com/gabrielafonso/ipb/castelobranco/ui/screens/main/MainActivity.kt
+++ app/src/main/java/com/gabrielafonso/ipb/castelobranco/ui/screens/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
